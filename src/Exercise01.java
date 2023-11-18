7dc41302da585386caa21f64c5d707f3f86908f9
YuutaMatsuo <kuro1152@yahoo.co.jp>
2023-11-18 13:44:11 +0900

    じゃんけんゲーム完成

--- src/Exercise01.java
+++ src/Exercise01.java
@@ -11,6 +11,44 @@ public class Exercise01 {
 //		【前提条件】
 //		・キーボードからの入力値は、1~3の整数以外は入力されないものとする。
 
+		// 変数を宣言
+
+		String[] rpsArray = { "グー", "チョキ", "パー" };
+		int player;
+		int cpu;
+		String result;
+
+		do {
+			System.out.println("じゃんけんゲームをはじめます。");
+			System.out.print("出したい手を選んでください（1:グー 2:チョキ 3:パー）\n>>");
+			
+			//player: コンソールからの入力を受け取る cpu: ランダムに１～３の値を生成する
+			player = new java.util.Scanner(System.in).nextInt();
+			cpu = new java.util.Random().nextInt(1,3);
+			//じゃんけんの処理
+			result = rockPaperScissors(player,cpu);
+			//結果を出力
+			System.out.println("あなた：" + rpsArray[cpu-1] + " わたし：" + rpsArray[player-1]);
+			System.out.println(result);
+			
+		} while (player == cpu);
+	}
+	
+	//じゃんけんの勝ち負け処理 (１：グー２：チョキ３：パー とした時のplayer - cpuの値を計算し勝ち負けを判定する）
+	public static String rockPaperScissors(int player,int cpu) {
+		String result;
+		int winOrLoss = player - cpu;
+		switch(winOrLoss) {
+		case -1:
+		case 2:
+			return result = "あなたの勝ちです！";
+		case 1:
+		case -2:
+			return result = "ざんねん！あなたの負けです。";
+		case 0:
+			return result = "あいこですね。もう一回！";
+		}
+		return result = "入力に誤りがあります";
 	}
 
 }
