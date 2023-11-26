5e6398cb87c46d7ae37799d518182ade6fac53de
kuro115 <126228724+YuutaMatsuo@users.noreply.github.com>
2023-11-26 21:55:41 +0900

    練習問題完成

--- src/Exercise05.java
+++ src/Exercise05.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Exercise05 {
 
@@ -10,7 +11,36 @@ public class Exercise05 {
 //
 //		【前提条件】
 //		・キーボードから受け付ける値は必ず正の整数が入力されるものとする。
-
+		
+		System.out.print("クラスの人数を入力してください>>");
+		int people = new Scanner(System.in).nextInt();
+		
+		int[][] score = new int[people][5];
+		int[] sum = new int[people];
+		int[] average = new int[5];
+		int[] subjectSum = new int[5];
+		String[] subject = {"国語","算数","理科","社会","英語"};
+		
+		for (int i = 0; i < people; i++) {
+			System.out.println((i + 1) + "人目の各科目の点数を入力してください。");
+			for (int j = 0; j < 5; j++) {
+				System.out.print(subject[j] + ">>");
+				score[i][j] = new Scanner(System.in).nextInt();
+				sum[i] += score[i][j];
+				subjectSum[j] += score[i][j];
+			}
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			average[i] = subjectSum[i] / people;
+		}
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(subject[i] + "の平均点：" + average[i] );
+		}
+		
+		for (int i = 0; i < people; i++) {
+			System.out.println((i + 1) + "人目の合計点：" + sum[i]);
+		}
 	}
-
 }
